6ddef5f29ee194103223e31f41cd12fa758bb23d
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-04-08 09:19:45 +0300

    initial commit

---
+++ src/main/java/ee/itcollege/taltechcars/TaltechCarsApplication.java
@@ -0,0 +1,13 @@
+package ee.itcollege.taltechcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaltechCarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaltechCarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/itcollege/taltechcars/TaltechCarsApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.itcollege.taltechcars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaltechCarsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
