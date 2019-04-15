58f3caf0aa5b8f5f7581b614d80e8c8fd128cd94
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-04-15 08:41:49 +0300

    List of cars testtemplate example

--- src/test/java/ee/itcollege/taltechcars/controller/CarControllerTest.java
+++ src/test/java/ee/itcollege/taltechcars/controller/CarControllerTest.java
@@ -6,9 +6,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +20,15 @@ import static org.junit.Assert.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CarControllerTest {
 
+    public static final ParameterizedTypeReference<List<Car>> LIST_OF_CARS = new ParameterizedTypeReference<List<Car>>() {
+    };
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     public void findAll() {
+        restTemplate.exchange("/car", HttpMethod.GET, null, LIST_OF_CARS);
         restTemplate.getForEntity("/car/1", Car.class);
     }
 
