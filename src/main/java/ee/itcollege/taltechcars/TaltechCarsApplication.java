e959f9bb75584301dc7cc7edc6949af378a455bd
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-04-29 08:59:57 +0300

    Add saving a lease

--- src/main/java/ee/itcollege/taltechcars/TaltechCarsApplication.java
+++ src/main/java/ee/itcollege/taltechcars/TaltechCarsApplication.java
@@ -1,8 +1,10 @@
 package ee.itcollege.taltechcars;
 
 import ee.itcollege.taltechcars.model.Car;
+import ee.itcollege.taltechcars.model.Lease;
 import ee.itcollege.taltechcars.model.User;
 import ee.itcollege.taltechcars.repository.CarRepository;
+import ee.itcollege.taltechcars.repository.LeaseRepository;
 import ee.itcollege.taltechcars.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,22 +19,25 @@ public class TaltechCarsApplication {
     }
 
     @Bean
-    public CommandLineRunner initCars(CarRepository carRepository, UserRepository userRepository) {
+    public CommandLineRunner initCars(CarRepository carRepository,
+                                      UserRepository userRepository,
+                                      LeaseRepository leaseRepository) {
         return (args) -> {
             // save a couple of cars
-            carRepository.save(new Car("111111111", 1999, "VW Golf"));
+            Car savedVwGolf = carRepository.save(new Car("111111111", 1999, "VW Golf"));
             carRepository.save(new Car("222222222", 2008, "Audi A6"));
             carRepository.save(new Car("333333333", 2014, "BMW 318d"));
             carRepository.save(new Car("444444444", 2016, "Volvo V50"));
             carRepository.save(new Car("555555555", 2018, "Audi A6"));
 
             // save a couple of students
-            userRepository.save(new User("SweetJill14", "ICS0011"));
+            User jill = userRepository.save(new User("SweetJill14", "ICS0011"));
             userRepository.save(new User("Chloe123", "ICS0012"));
             userRepository.save(new User("KimTheDragon", "ICS0013"));
             userRepository.save(new User("DavidDavid", "ICS0014"));
             userRepository.save(new User("Mich3ll3", "ICS0015"));
 
+            leaseRepository.save(new Lease(savedVwGolf.getId(), jill.getId()));
             //todo save a lease
         };
     }
--- src/main/java/ee/itcollege/taltechcars/model/Lease.java
+++ src/main/java/ee/itcollege/taltechcars/model/Lease.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -12,10 +13,67 @@ public class Lease {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotNull
     private Long car;
+    @NotNull
     private Long user;
     private LocalDate startDate;
     private LocalDate endDate;
     private LocalDate returnDate;
 
+    public Lease() {
+    }
+
+    public Lease(Long car, Long user) {
+        this.car = car;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCar() {
+        return car;
+    }
+
+    public void setCar(Long car) {
+        this.car = car;
+    }
+
+    public Long getUser() {
+        return user;
+    }
+
+    public void setUser(Long user) {
+        this.user = user;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
 }
