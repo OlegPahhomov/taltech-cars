f1c5c66477b189f8cf1ea1b1d811dc28e4b72a94
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-05-04 21:09:30 +0300

    Fix type

--- src/main/java/ee/itcollege/taltechcars/controller/LeaseController.java
+++ src/main/java/ee/itcollege/taltechcars/controller/LeaseController.java
@@ -7,7 +7,8 @@ public class LeaseController {
     //todo 3 user can return a car
     //todo 4 user can see his leases
 
-    //todo 5 (not in this class) add a cron job and custom properties for fun
-    //todo 6 (not in this class) add a api request for sth
+    //todo 5 (not in this class) add a cron job
+    // 5.1 and custom properties for fun
+    //todo 6 (not in this class) add an api request for sth
     //todo 7 (import a csv file)
 }
