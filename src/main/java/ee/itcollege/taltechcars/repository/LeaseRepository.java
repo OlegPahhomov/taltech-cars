4eb7670de4b8af79979a8be32e26ecee7854ab36
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-05-06 09:32:00 +0300

    Lease saving logic validations

--- src/main/java/ee/itcollege/taltechcars/repository/LeaseRepository.java
+++ src/main/java/ee/itcollege/taltechcars/repository/LeaseRepository.java
@@ -2,6 +2,7 @@ package ee.itcollege.taltechcars.repository;
 
 import ee.itcollege.taltechcars.model.Car;
 import ee.itcollege.taltechcars.model.Lease;
+import ee.itcollege.taltechcars.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface LeaseRepository extends JpaRepository<Lease, Long> {
 
     List<Lease> findByCar(Car car);
+
+    List<Lease> findByUserAndReturnDateNull(User user);
 }
--- src/main/java/ee/itcollege/taltechcars/service/LeaseService.java
+++ src/main/java/ee/itcollege/taltechcars/service/LeaseService.java
@@ -9,11 +9,14 @@ import ee.itcollege.taltechcars.repository.LeaseRepository;
 import ee.itcollege.taltechcars.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Service
 public class LeaseService {
@@ -29,19 +32,38 @@ public class LeaseService {
         Lease dbLease = new Lease();
 
         //todo validate lease.getUser is not null
-        Optional<User> dbUserOp = userRepository.findById(lease.getUser().getId());
-        User dbUser = dbUserOp.orElseThrow(RuntimeException::new);
-        dbLease.setUser(dbUser);
-
-        Optional<Car> dbCarOp = carRepository.findById(lease.getCar().getId());
-        Car dbCar = dbCarOp.orElseThrow(RuntimeException::new); //todo return 400
-        dbLease.setCar(dbCar);
+        dbLease.setUser(getValidUser(lease));
+        Car car = getValidCar(lease);
+        dbLease.setCar(car);
 
         dbLease.setStartDate(LocalDate.now());
         dbLease.setEndDate(LocalDate.now().plusWeeks(1));
 
+        car.setLeased(true);
+        carRepository.save(car);
+
         Lease saved = leaseRepository.save(dbLease);
         return convert(saved);
+    }
+
+    private User getValidUser(LeaseDto lease) {
+        Optional<User> dbUserOp = userRepository.findById(lease.getUser().getId());
+        User dbUser = dbUserOp.orElseThrow(RuntimeException::new);
+        List<Lease> existingLeases = leaseRepository.findByUserAndReturnDateNull(dbUser);
+        if (!existingLeases.isEmpty()){
+            throw new ResponseStatusException(BAD_REQUEST, "user already has a car leased");
+        }
+        return dbUser;
+    }
+
+    private Car getValidCar(LeaseDto lease) {
+        Optional<Car> dbCarOp = carRepository.findById(lease.getCar().getId());
+        Car dbCar = dbCarOp.orElseThrow(RuntimeException::new); //todo return 400
+
+        if (dbCar.isLeased()){
+            throw new ResponseStatusException(BAD_REQUEST, "car is already leased");
+        }
+        return dbCar;
     }
 
     public List<LeaseDto> findAll() {
@@ -56,5 +78,9 @@ public class LeaseService {
         leaseDto.setCar(lease.getCar());
         leaseDto.setUser(lease.getUser());
         return leaseDto;
+    }
+
+    private ResponseStatusException badRequest() {
+        return new ResponseStatusException(BAD_REQUEST, "id doesnt exist");
     }
 }
