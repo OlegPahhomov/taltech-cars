10e9ad2ca11c35f51e2b2eaa8f5290c67180789b
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-05-13 09:14:12 +0300

    @Scheduled example

---
+++ src/main/java/ee/itcollege/taltechcars/config/CronConfig.java
@@ -0,0 +1,9 @@
+package ee.itcollege.taltechcars.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class CronConfig {
+}
--- src/main/java/ee/itcollege/taltechcars/service/NotificationManager.java
+++ src/main/java/ee/itcollege/taltechcars/service/NotificationManager.java
@@ -1,15 +1,20 @@
 package ee.itcollege.taltechcars.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationManager {
 
     //add repo
+    @Autowired
+    private LeaseService leaseService;
 
-    //todo doMagic for cron to start working
-    public void something() {
-        System.out.println(12);
-        //repo.doMoreMagic
+    @Scheduled(fixedDelay = 1000)
+    public void scheduleFixedDelayTask() {
+        System.out.println(
+                "Fixed delay task - " + System.currentTimeMillis() / 1000);
     }
+
 }
