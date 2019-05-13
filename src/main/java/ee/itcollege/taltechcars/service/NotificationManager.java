bb744b3914b8b26c24af083c622b6ca2de486249
oleg.pahhomov <oleg.pahhomov@netgroup.ee>
2019-05-13 09:02:13 +0300

    Add notification manager

---
+++ src/main/java/ee/itcollege/taltechcars/service/NotificationManager.java
@@ -0,0 +1,15 @@
+package ee.itcollege.taltechcars.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationManager {
+
+    //add repo
+
+    //todo doMagic for cron to start working
+    public void something() {
+        System.out.println(12);
+        //repo.doMoreMagic
+    }
+}
